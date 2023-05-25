a49be9d5c1004d528a20cfdf794ad9d8308d55a8
shgus1224 <shgus1224@naver.com>
2023-05-26 01:52:28 +0900

    chore: 전역 style, type 값 추가

---
+++ src/pages/Start.styles.ts
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+import theme from '../styles/theme';
+
+export const MainContainer = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 412px;
+  height: 100vh;
+  background-color: ${theme.colors.burgundy};
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+`;
+
+export const LogoImage = styled.img`
+  width: 100%;
+  height: 250px;
+  margin: 0 auto;
+`;
+
+export const NextButton = styled.div`
+  position: absolute;
+  width: 330px;
+  height: 50px;
+  bottom: 40px;
+  margin: 0 auto;
+  background-color: white;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.text`
+  font-size: 20px;
+  ${theme.typography.semibold01};
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react';
+
+const colors = {
+  burgundy: '#9E1915',
+};
+
+const typography = {
+  semibold01: css`
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 130%;
+  `,
+};
+
+const theme = {
+  colors,
+  typography,
+};
+
+export default theme;
