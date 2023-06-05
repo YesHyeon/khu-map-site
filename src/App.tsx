dee87e7a763f86f642013429f2eceb1fd2ffc1bb
shgus1224 <shgus1224@naver.com>
2023-06-05 23:54:33 +0900

    chore: 페이지 파일 루트 변경 및 package 파일 업로드

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Start from './pages/Start';
-import Main from './pages/Main';
+import Start from './pages/Start/Start';
+import Main from './pages/Main/Main';
 
 function App() {
   return (
